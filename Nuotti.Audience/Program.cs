dd2b24dc889df89187b310063c9e45b6d84634cf
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2025-09-28 18:10:09 +0200

    implement MudBlazor components for improved UI and navigation

--- Nuotti.Audience/Program.cs
+++ Nuotti.Audience/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Nuotti.Audience;
 using Nuotti.Audience.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +12,8 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+
+builder.Services.AddMudServices();
 
 // AudienceHubClient depends on HttpClient (scoped), so it must be scoped too
 builder.Services.AddScoped<AudienceHubClient>();
